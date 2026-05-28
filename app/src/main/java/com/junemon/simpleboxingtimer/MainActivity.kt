497569c4d93cbf05b9a690202fd529f7fb186fed
ian damping <iandamping92@gmail.com>
2026-05-28 15:56:44 +0800

    feat(ui): setup multi screen orientation support
    - Renaming BoxingTimerScreen.kt into BoxingTimerRoute.kt
    - Create PortraitBoxingTimerScreen.kt to support portrait mode
    - Create LandscapeBoxingTimerScreen.kt to support landscape mode
    - remove screenOrientation in manifest

--- app/src/main/java/com/junemon/simpleboxingtimer/MainActivity.kt
+++ app/src/main/java/com/junemon/simpleboxingtimer/MainActivity.kt
@@ -5,21 +5,17 @@ import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.ui.Modifier
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
-import com.junemon.simpleboxingtimer.ui.screen.BoxingTimerScreen
+import com.junemon.simpleboxingtimer.ui.screen.BoxingTimerRoute
 import com.junemon.simpleboxingtimer.ui.theme.TimerTheme
-import com.junemon.simpleboxingtimer.viewmodel.BoxingTimerViewModel
-import com.junemon.simpleboxingtimer.viewmodel.DataProviderViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 class MainActivity : ComponentActivity() {
-
-    private val boxingVm: BoxingTimerViewModel by viewModels()
-    private val dataVm: DataProviderViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         installSplashScreen()
@@ -28,11 +24,9 @@ class MainActivity : ComponentActivity() {
         window.addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
         setContent {
             TimerTheme {
-                BoxingTimerScreen(
+                BoxingTimerRoute(
                     modifier = Modifier
                         .fillMaxSize(),
-                    timerVm = boxingVm,
-                    dataVm = dataVm
                 )
             }
         }
---
+++ app/src/main/java/com/junemon/simpleboxingtimer/ui/screen/LandscapeBoxingTimerScreen.kt
@@ -0,0 +1,123 @@
+package com.junemon.simpleboxingtimer.ui.screen
+
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.Dimension
+import com.google.android.gms.ads.AdView
+import com.junemon.simpleboxingtimer.util.TimerConstant
+import com.junemon.simpleboxingtimer.viewmodel.RestTime
+import com.junemon.simpleboxingtimer.viewmodel.TimerClassification
+
+@Composable
+fun LandscapeBoxingTimerScreen(
+    modifier: Modifier = Modifier,
+    adView: AdView,
+    isTimerRunning: Boolean,
+    isRadioButtonEnabled: Boolean,
+    isResting: Boolean,
+    pauseTime: Long,
+    timeTicking: Long?,
+    currentRound: Int,
+    whichRoundRunning: Int,
+    restTimes: List<RestTime>,
+    timerClassifications: List<TimerClassification>,
+    setRestTime: (Int) -> Unit,
+    setRoundTime: (Int) -> Unit,
+    setWhichRound: (Int) -> Unit,
+    setWarningValue: (Int) -> Unit,
+    cancelAllTimer: () -> Unit,
+    startCounting: () -> Unit,
+    resetAllTimer: () -> Unit,
+) {
+    ConstraintLayout(modifier = modifier.verticalScroll(rememberScrollState())) {
+        val (timerRef, selectingTimeAndRoundRef, warningButtonRef, intervalTimerButtonRef, bannerAddViewRef) = createRefs()
+        val topGuideLine = createGuidelineFromTop(0.1f)
+        val bottomGuideLine = createGuidelineFromBottom(0.2f)
+        val startGuideLine = createGuidelineFromStart(0.1f)
+        val endGuideLine = createGuidelineFromEnd(0.1f)
+        val centerHorizontalGuideline = createGuidelineFromStart(0.5f)
+
+        if (isTimerRunning) {
+            TimerInformationSection(
+                modifier = Modifier.constrainAs(timerRef) {
+                    top.linkTo(topGuideLine)
+                    bottom.linkTo(bottomGuideLine)
+                    start.linkTo(startGuideLine)
+                    end.linkTo(centerHorizontalGuideline)
+                    width = Dimension.wrapContent
+                },
+                isResting = isResting,
+                timeTickingForText = timeTicking,
+                currentRound = currentRound,
+                whichRoundRunning = whichRoundRunning
+            )
+        } else {
+            if (pauseTime != TimerConstant.DEFAULT_LONG_VALUE) {
+                TimerInformationSection(
+                    modifier = Modifier.constrainAs(timerRef) {
+                        top.linkTo(topGuideLine)
+                        bottom.linkTo(bottomGuideLine)
+                        start.linkTo(startGuideLine)
+                        end.linkTo(centerHorizontalGuideline)
+                        width = Dimension.wrapContent
+                    }, isResting = isResting,
+                    timeTickingForText = timeTicking,
+                    currentRound = currentRound,
+                    whichRoundRunning = whichRoundRunning
+                )
+            } else IntervalSetupSection(
+                modifier = Modifier.constrainAs(
+                    selectingTimeAndRoundRef
+                ) {
+                    top.linkTo(topGuideLine)
+                    bottom.linkTo(bottomGuideLine)
+                    start.linkTo(startGuideLine)
+                    end.linkTo(centerHorizontalGuideline)
+                    width = Dimension.wrapContent
+                },
+                setRestTime = setRestTime,
+                setRoundTime = setRoundTime,
+                setWhichRound = setWhichRound,
+                timerClassifications = timerClassifications
+            )
+        }
+
+        WarningTimeRadioSection(
+            modifier = Modifier.constrainAs(warningButtonRef) {
+                top.linkTo(topGuideLine)
+                start.linkTo(centerHorizontalGuideline)
+                end.linkTo(endGuideLine)
+                bottom.linkTo(bottomGuideLine)
+                width = Dimension.wrapContent
+            },
+            pauseTime = pauseTime,
+            isRadioButtonEnabled = isRadioButtonEnabled,
+            setWarningValue = setWarningValue,
+            restTimes = restTimes
+        )
+
+        IntervalTimerButtonSection(
+            modifier = Modifier.constrainAs(intervalTimerButtonRef) {
+                top.linkTo(warningButtonRef.bottom)
+                start.linkTo(centerHorizontalGuideline)
+                bottom.linkTo(bottomGuideLine)
+                end.linkTo(endGuideLine)
+                width = Dimension.fillToConstraints
+            },
+            isTimerRunning = isTimerRunning,
+            cancelAllTimer = cancelAllTimer,
+            startCounting = startCounting,
+            resetAll = resetAllTimer
+        )
+
+        AdsBannerScreen(adView, modifier = Modifier.constrainAs(bannerAddViewRef) {
+            top.linkTo(parent.top)
+            start.linkTo(parent.start)
+            end.linkTo(parent.end)
+            width = Dimension.fillToConstraints
+        })
+    }
+}
